3b5c78f62dce208ffe8d314c24eb1b07ff7e648b
asifur77 <asifur77@gmail.com>
2016-11-14 20:20:43 +0600

    RA|cleaned the repo

--- opensrp-api/src/main/java/org/opensrp/api/contract/AuthorizationHandler.java
+++
@@ -1,8 +0,0 @@
-package org.opensrp.api.contract;
-
-import org.opensrp.api.domain.User;
-
-public interface AuthorizationHandler<T> {
-
-	boolean verifyAuthorization(User user, T service) throws IllegalAccessException;
-}
